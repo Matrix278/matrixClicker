497bb0d7ff8ef09b3b48a4bfd9401f25b37b8fd2
Martin Sidorov <35573364+Matrix278@users.noreply.github.com>
2020-03-30 23:02:23 +0300

    Add entire project game

---
+++ Assets/Scripts/ClickObj.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickObj : MonoBehaviour
+{
+    private bool move;
+
+    private Vector2 randomVector;
+
+    private void Update()
+    {
+        if (!move) return;
+        transform.Translate(randomVector * Time.deltaTime);
+    }
+
+    public void StartMotion(int scoreIncrease)
+    {
+        transform.localPosition = Vector2.zero;
+        GetComponent<Text>().text = "+" + scoreIncrease;
+        randomVector = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5));
+        move = true;
+    }
+	
+}
